6bee6d4d9c46a0c6e2c0991fa16d5969be16c64b
Amir01010 <amiralinasrishandiz@gmail.com>
2021-11-24 18:58:21 +0100

    first comment

---
+++ boiletemple/scrip.js
@@ -0,0 +1,56 @@
+
+
+//variablen>altaer standard
+
+
+var name1 ="jomi";
+var name2='Amir';
+
+//variablen 
+//neuer standart
+let firstname ="maxim";
+let lastname = "serjio";
+
+
+//konstanten
+const geburtort = 'Berlin';
+console.log( firstname +" " + lastname +" " +  geburstort);
+
+
+console.log( firstname +" " + lastname +" " +  geburstort);
+
+
+let a =2;
+let b =5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+let ergebnis = a + b * 10;
+console.log(ergebnis1);
+let ergebnis2 =(a+b)*10;
+console.log(ergebnis2);
+
+
+let city = "  Amsterdam  ";
+console.log(city.length);
+console.log(city);
+//whitespace entfernten
+console.log(city,trim());
+console.log(city,trim().length);
+let test = 1;
+console.log(test);
+
+// TYPE OPPERATOR
+let test = 1;
+console.log(typeof test);
+
+
+let faivoritefood ='pizza';
+let faivoriteDrinke ='Bier';
+
+
+
+
+
+
